b851dfc8d705f3fae9d36733179a0ec24a40cf08
saim-ahmed-f <saim.ahmed.f@gmail.com>
2022-04-01 02:10:34 +0530

    final deploy 4

--- shipping_detail/models.py
+++ shipping_detail/models.py
@@ -8,11 +8,12 @@ class shipping_detail(models.Model):
     
     name = models.CharField(max_length=50)
     email = models.EmailField()
-    phone = models.IntegerField()
+    phone = models.PhoneNumberField()
     address = models.TextField()
     city = models.CharField(max_length=50)
     pincode = models.PositiveIntegerField()
     state = models.CharField(max_length=50)  
+    
 
     def __str__(self):
         return self.name
